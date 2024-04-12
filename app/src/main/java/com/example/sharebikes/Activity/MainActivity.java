f729bb894a12ac4c2c2a4dfd07a78151e680393f
shallwet <527936582@qq.com>
2024-04-12 23:07:24 +0800

    feat:增加高德地图实现定位功能

--- app/src/main/java/com/example/sharebikes/Activity/MainActivity.java
+++ app/src/main/java/com/example/sharebikes/Activity/MainActivity.java
@@ -2,118 +2,63 @@ package com.example.sharebikes.Activity;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.core.view.GravityCompat;
-
-//import com.amap.api.location.AMapLocationClient;
-
-import com.amap.api.maps2d.AMap;
-import com.amap.api.maps2d.MapView;
-import com.google.android.material.navigation.NavigationView;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapsInitializer;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.example.sharebikes.R;
-
-
+import com.amap.api.maps.MapView;
+import java.util.List;
 public class MainActivity extends BaseActivity {
-
-    private DrawerLayout drawerLayout; // 滑动菜单
-    private Toolbar toolbar; // 工具栏
-    private NavigationView navView; // 导航视图
-    private MapView mapView; // 地图视图
+    MapView mMapView = null;
+    private AMap aMap = null; // 声明AMap对象
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MapsInitializer.updatePrivacyShow(this,true,true);
+        MapsInitializer.updatePrivacyAgree(this,true);
         setContentView(R.layout.activity_main);
-        // 隐私协议
-//        MapsInitializer.updatePrivacyAgree(this,true);
-//        MapsInitializer.updatePrivacyShow(this,true,true);
-
-        // 初始化组件
-        initViews();
-
-        // 设置Toolbar作为ActionBar
-        setSupportActionBar(toolbar);
-        // 注意：如果你的应用目标API级别是Android Lollipop（API 21）或更高，
-        // 并且你想要在Toolbar上显示导航图标（如汉堡图标），你可能还需要调用以下代码：
-        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        // getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu); // 替换为你的汉堡菜单图标资源
-        // 高德地图SDK初始化之前调用
-
-        // 初始化地图
-        mapView.onCreate(savedInstanceState); // 必须调用
-        AMap aMap = mapView.getMap(); // 获取地图控制器对象
-
-        // 设置导航视图头部布局点击事件
-        View headerView = navView.getHeaderView(0);
-        headerView.setOnClickListener(view -> showMsg("点击了头部布局"));
-        // 设置导航视图菜单点击事件
-        navView.setNavigationItemSelectedListener(item -> {
-            int id = item.getItemId();
-            if (id == R.id.item_order) {
-                showMsg("点击了我的订单");
-            } else if (id == R.id.item_wallet) {
-                showMsg("点击了我的钱包");
-            } else if (id == R.id.item_guidance) {
-                showMsg("点击了使用指南");
-            }
-            // 点击任何项之后关闭抽屉
-            drawerLayout.closeDrawer(GravityCompat.START);
-            return true;
-        });
-
-        // 设置工具栏按钮点击事件，打开抽屉
-        toolbar.setNavigationOnClickListener(view -> drawerLayout.openDrawer(GravityCompat.START));
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);
+        // 初始化地图控制器对象
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+        }
+        MyLocationStyle myLocationStyle;
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        //aMap.getUiSettings().setMyLocationButtonEnabled(true); //设置默认定位按钮是否显示，非必需设置。
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        aMap.moveCamera(com.amap.api.maps.CameraUpdateFactory.zoomTo(15));
     }
-
-    private void initViews() {
-        drawerLayout = findViewById(R.id.drawer_layout);
-        toolbar = findViewById(R.id.toolbar);
-        navView = findViewById(R.id.nav_view);
-        mapView = (MapView)findViewById(R.id.map);
-    }
-
-    /**
-     * Toast提示
-     * @param msg 内容
-     */
-    private void showMsg(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mapView.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
     }
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
     }
 
 }
+
---
+++ app/src/main/java/com/example/sharebikes/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.sharebikes;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+
+public class DemoApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
+
